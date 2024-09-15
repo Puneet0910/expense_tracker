f41e814ca60900904f0cc256995dd756ef314a15
Puneet0910 <nainwalpuneet@gmail.com>
2024-09-15 11:15:24 +0530

    transactionAdded

--- ExpenseTracker/controllers/expense.js
+++ ExpenseTracker/controllers/expense.js
@@ -1,7 +1,10 @@
 const Expense = require("../models/expense");
 const User = require("../models/user");
+const sequelize = require("../util/database");
 
-exports.addExpense = (req, res, next) => {
+exports.addExpense = async (req, res, next) => {
+  const transaction = await sequelize.transaction();
+
   const { amount, description, category } = req.body;
 
   if (
@@ -14,11 +17,18 @@ exports.addExpense = (req, res, next) => {
       .json({ success: false, message: "Parameters missing" });
   }
 
-  Expense.create({ amount, description, category, userId: req.user.id })
-    .then((expense) => {
+  Expense.create(
+    { amount, description, category, userId: req.user.id },
+    { transaction: transaction }
+  )
+
+    .then(async (expense) => {
+      await transaction.commit();
+      // const totalExpenses = Number(req.user.totalExpenses) + Number(amount);
       return res.status(201).json({ expense, success: true });
     })
-    .catch((err) => {
+    .catch(async (err) => {
+      await transaction.rollback();
       return res.status(500).json({ success: false, error: err });
     });
 };
@@ -34,15 +44,27 @@ exports.getExpenses = async (req, res, next) => {
   // })
 };
 
-exports.deleteExpense = (req, res, next) => {
-  const expId = req.params.id;
+// exports.deleteExpense = (req, res, next) => {
+//   const expId = req.params.id;
 
-  Expense.destroy({ where: { id: expId } })
+//   Expense.destroy({ where: { id: expId } })
+//     .then((response) => {
+//       res.status(203).json({ success: true, message: "Deleted Successfully" });
+//     })
+//     .catch((err) => {
+//       res.status(400).json({ error: err });
+//     });
+// };
+
+exports.deleteExpense = (req, res, next) => {
+  const exId = req.params.id;
+
+  Expense.destroy({ where: { id: exId } })
     .then((response) => {
+      console.log(response);
       res.status(203).json({ success: true, message: "Deleted Successfully" });
     })
     .catch((err) => {
       res.status(400).json({ error: err });
     });
 };
-
